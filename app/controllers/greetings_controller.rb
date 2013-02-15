2ba1fb9fefd35f6baeb7a63d480cb0c0a9195547
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-02-16 00:09:42 +0100

    Show some routing and basic page handling
    Added some defaults. Also try `greetings/Some Random Name`.

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,7 +1,9 @@
 class GreetingsController < ApplicationController
   def index
+	@message = "No greetings today."
   end
 
   def hello
+	@message = "Hello " + ( params['name'] || "you!" )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ProgrammingLife::Application.routes.draw do
+ 
+  get 'greetings/hello'
+  get 'greetings' => 'greetings#index'
+  get 'greetings/:name' => 'greetings#hello'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ ProgrammingLife::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'greetings#hello'
 
   # See how all your routes lay out with "rake routes"
 
