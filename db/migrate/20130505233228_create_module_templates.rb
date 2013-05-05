8593ec4e9fb6a4e2bb68b13e3e5f1a2ff9b2b915
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-05-06 01:55:09 +0200

    Migrations

--- db/migrate/20130505233228_create_module_templates.rb
+++ db/migrate/20130505233228_create_module_templates.rb
@@ -1,8 +1,9 @@
 class CreateModuleTemplates < ActiveRecord::Migration
   def change
     create_table :module_templates do |t|
-
+	  t.integer :id
       t.timestamps
     end
+	add_index :module_templates, :id, :unique => true
   end
 end
--- db/migrate/20130505233612_create_module_instances.rb
+++ db/migrate/20130505233612_create_module_instances.rb
@@ -1,8 +1,9 @@
 class CreateModuleInstances < ActiveRecord::Migration
   def change
     create_table :module_instances do |t|
-
+	  t.integer :id
       t.timestamps
     end
+	add_index :module_instances, :id, :unique => true
   end
 end
