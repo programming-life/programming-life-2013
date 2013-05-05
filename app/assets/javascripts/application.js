1fed98191564902010913f639a2be25c25ea6efa
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-05-05 23:43:46 +0200

    Re-added raphael

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require underscore
+//= require big.min
 //
 //= require MVC
 //
@@ -23,8 +24,7 @@
 //= require_tree .
 //
 //= require bootstrap
-//= require raphael-2.1.0
+//= require raphael.min
 //= require chart
 //= require md5.min
-//
 //= require numeric-1.2.6
