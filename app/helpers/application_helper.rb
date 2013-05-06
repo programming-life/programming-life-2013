33297d5df7bb3eeb59317a7e32205e1287206e9f
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-05-06 17:54:51 +0200

    Added listify helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def listify( list )
+		result = case list.count
+			when 0 then 'none'
+			when 1 then list.first
+			else list.first( list.count - 1 ).join( ', ') + ' and ' + list.last
+			end
+		return result
+	end
 end
