539751f4c67c674dfea71b5bd4df7eb3a5eec49a
Vincent Robbemond <vincentrobbemond@live.nl>
2013-05-26 20:08:13 +0200

    Added reports view tests

--- test/integration/reports_view_test.rb
+++ test/integration/reports_view_test.rb
@@ -6,9 +6,30 @@ require 'selenium-webdriver'
 class ReportsViewTests < ActionDispatch::IntegrationTest
 
 	def setup
-		driver = Selenium::WebDriver.for :firefox
-		wait = Selenium::WebDriver::Wait.new :timeout => 10
+		@driver = Selenium::WebDriver.for :firefox
+		@wait = Selenium::WebDriver::Wait.new :timeout => 10
 		@driver.navigate.to "http://life.jstfy.com/reports"
+	end
+
+	test "navigate to show-reports view" do
+		button = @driver.find_element(:class, "icon-search")
+		button.click
+		url = @driver.current_url
+		assert_equal url, "http://life.jstfy.com/reports/1"
+	end
+
+	test "navigate to cell view" do
+		link = @driver.find_element(:tag_name, "a")
+		link.click
+		url = @driver.current_url
+		assert_equal url, "http://life.jstfy.com/cells/1"
+	end
+
+	test "navigate to new-report view" do
+		button = @driver.find_element(:class, "icon-plus-sign")
+		button.click
+		url = @driver.current_url
+		assert_equal url, "http://life.jstfy.com/reports/new"
 	end
 
 	def teardwown
