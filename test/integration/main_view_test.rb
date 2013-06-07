35395caf29d84473cfd97d8a35e8001ab5b0a8a5
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-06-08 01:25:33 +0200

    Wait until stuff is displayed

--- test/integration/main_view_test.rb
+++ test/integration/main_view_test.rb
@@ -29,37 +29,59 @@ class MainViewTest < ActionDispatch::IntegrationTest
 	#Mouseover event on class module-hitbox
 	#Check if popover showed up on mouseover
 	test "module mouseover" do
+		sleep( 2 )
 		el = @wait.until { @driver.find_element(:class, "cellgrowth-hitbox") }
-		@driver.mouse.move_to(el)
-		pop = @wait.until { @driver.find_element(:class, "cellgrowth-box") }
-		assert pop.displayed?
+		displayed = false
+		if @wait.until { el.displayed? }
+			@driver.mouse.move_to(el)
+			pop = @wait.until { @driver.find_element(:class, "cellgrowth-box") }
+			displayed = @wait.until { pop.displayed? }
+		end
+		assert displayed
 	end
 
 	#Click on module
 	#Check if edit popover showed up
 	test "open edit popover" do
+		sleep( 2 )
 		el = @wait.until { @driver.find_element(:class, "module-hitbox") }
-		el.click
-		pop = @wait.until { @driver.find_element(:class, "popover") }
-		assert pop.displayed?
+		displayed = false
+		if @wait.until { el.displayed? }
+			el.click
+			pop = @wait.until { @driver.find_element(:class, "selected") }
+			displayed = @wait.until { pop.displayed? }
+		end
+		assert displayed
 	end
 
 	#Click on dummy module
 	#Check wether a module has been added
 	test "add a module" do
-		el = @wait.until { @driver.find_element(:class, "inactive") }
-		el.click
-		newmodule = @wait.until { @driver.find_element(:class, "module-hitbox") }
-		assert newmodule.displayed?
+		sleep( 2 )
+		els = @wait.until { @driver.find_elements(:class, "inactive") }
+		displayed = false
+		for el_pot in els
+			if el_pot.displayed?
+				el = el_pot
+				break
+			end
+		end
+		if el and @wait.until { el.displayed? }
+			el.click
+			newmodule = @wait.until { @driver.find_element(:class, "module-hitbox") }
+			displayed = @wait.until { newmodule.displayed? } 
+		end
+		assert displayed
 	end
 
 	#Click on Action History Pane
 	#Check if pane showed up
 	test "view the actionhistory pane" do
 		el = @wait.until { @driver.find_element(:class, "pane-button") }
-		el.click
 		pane = @wait.until { @driver.find_element(:class, "pane-left") }
-		assert_equal pane.attribute("class"), "pane pane-left extended"
+		assert_equal "pane pane-left extended", pane.attribute("class")
+		el.click
+		assert_equal "pane pane-left", pane.attribute("class")
 	end
 
 end
