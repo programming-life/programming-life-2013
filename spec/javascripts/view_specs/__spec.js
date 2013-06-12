d4b175044e2c4d198ac169df01cd0f5236a6cd72
Tung Phan <tungphan91@gmail.com>
2013-06-12 14:21:44 +0200

    Added new tests in base view spec

--- spec/javascripts/view_specs/__spec.js
+++ spec/javascripts/view_specs/__spec.js
@@ -1,15 +1,17 @@
 describe("Base", function() {
-	var view, paper, raphael, x, y;
+	var view, parent, paper, raphael, draw_x, draw_y, x, y;
 
 	beforeEach( function() {
-		x = 42;
-		y = 24;
-		raphael = Raphael(0, 0, 0, 0);
-		paper = {
-			set: jasmine.createSpy('set').andReturn(raphael.set()),
-			circle: jasmine.createSpy('circle').andReturn(raphael.circle(x, y, 0))
+		draw_x = 42;
+		draw_y = 24;
+		x = 10;
+		y = 20;
+		paper = Raphael(x, y, 30, 40);
+		parent = {
+			getViewPlacement: jasmine.createSpy('getViewPlacement'),
+			getAbsoluteCoords: jasmine.createSpy('getAbsoluteCoords')
 		};
-		view = new View.RaphaelBase(paper);
+		view = new View.RaphaelBase(paper, parent);
 	});
 
 	afterEach( function() {
@@ -32,7 +34,20 @@ describe("Base", function() {
 
 			beforeEach( function() {
 				spyOn(view, 'clear').andCallThrough();
-				contents = view.draw(x, y);
+				contents = view.draw(draw_x, draw_y);
+			});
+
+			it("should get the right bounding box", function() {
+				var BBox = {
+					x: draw_x,
+					y: draw_y,
+					x2: draw_x,
+					y2: draw_y,
+					width: 0,
+					height: 0
+				};
+
+				expect(view.getBBox()).toEqual(BBox);
 			});
 
 			it("should clear first", function() {
@@ -43,16 +58,30 @@ describe("Base", function() {
 				expect(contents).toBeDefined();
 			});
 
+			it("should be able to set a new x position", function() {
+				view.x = 40;
+				expect(view.x).toEqual(40);
+			});
+
+			it("should be able to set a new y position", function() {
+				view.y = 30;
+				expect(view.y).toEqual(30);
+			});
+
+			it("should be able to get the absolute coordinates", function() {
+				expect(view.getAbsoluteCoords(20, 40)).toEqual([x+20, y+40]);
+			});
+
 			describe("when moved to a new fixed position", function() {
 				beforeEach( function() {
 					spyOn(view, 'move').andCallThrough();
-					view.moveTo(x, y);
+					view.moveTo(12, 24, false);
 				});
 
 				it("should have been moved", function() {
 					expect(view.move).toHaveBeenCalled();
-					expect(view.x).toEqual(x);
-					expect(view.y).toEqual(y);
+					expect(view.x).toEqual(12);
+					expect(view.y).toEqual(24);
 				});
 			});
 
@@ -65,6 +94,20 @@ describe("Base", function() {
 				it("should do nothing when parent is null", function() {
 					expect(view.moveTo).not.toHaveBeenCalled();
 				});
+
+				it("should move to the new position", function() {
+					view.moveTo.reset();
+					parent = {
+						getViewPlacement: jasmine.createSpy('getViewPlacement').andReturn([2,4])
+					};
+					view._parent = parent;
+
+					view.setPosition(false);
+
+					expect(view.moveTo).toHaveBeenCalled();
+					expect(view.x).toEqual(2);
+					expect(view.y).toEqual(4);
+				})
 			});
 
 			describe("when redrawn", function() {
